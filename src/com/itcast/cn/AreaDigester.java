a32f11bf8eb8a58babdb5dc8e2c2e60aa6091ac6
JerrTom <j13579x@126.com>
2017-10-16 15:12:42 +0800

    commit

--- src/com/itcast/cn/AreaDigester.java
+++ src/com/itcast/cn/AreaDigester.java
@@ -13,13 +13,16 @@ public class AreaDigester {
 	public static void main(String[] args) {
 		AreaDigester ad = new AreaDigester();
 		List list = ad.digester().getAreaList();
-		System.out.println(list.toString());
+		System.out.println(list);
 	}
 	public ViewCache digester(){
 		Digester digester = new Digester();
 		
+		//Add an "object create" rule for the specified parameters.
 		digester.addObjectCreate("viewcache/areas", ViewCache.class);
+		// 指明匹配模式和要创建的类
 		digester.addObjectCreate("viewcache/areas/area", Area.class);
+		// 设置对象属性,与xml文件对应,不设置则是默认
 		digester.addBeanPropertySetter("viewcache/areas/area/id", "id");
 		digester.addBeanPropertySetter("viewcache/areas/area/parentId", "parentId");
 		digester.addBeanPropertySetter("viewcache/areas/area/name", "name");  
@@ -27,7 +30,9 @@ public class AreaDigester {
         digester.addBeanPropertySetter("viewcache/areas/area/ordering", "ordering");  
         digester.addBeanPropertySetter("viewcache/areas/area/zip", "zip");  
         digester.addBeanPropertySetter("viewcache/areas/area/phoneArea", "phoneArea");
+        // 当移动到下一个标签中时的动作
         digester.addSetNext("viewcache/areas/area", "addArea");
+        
         ViewCache vc = null;
         try {
 			vc = digester.parse(new File("viewcache.xml"));
